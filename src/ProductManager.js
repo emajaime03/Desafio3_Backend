0e6d50450709c27d813d5992fdca74d1a5b2dd3c
emajaime03 <emanico2903@gmail.com>
2024-02-18 20:46:11 -0300

    Agrego try/catch

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -7,30 +7,34 @@ class ProductManager {
     }
 
     async addProduct(title, description, price, thumbnail, stock, code) {
-        const products = await this.getProducts();
+        try {
+            const products = await this.getProducts();
 
-        if (!title || !description || !price || !thumbnail || !stock || !code) {
-            return "Debe completar todos los campos para agregar"
+            if (!title || !description || !price || !thumbnail || !stock || !code) {
+                return "Debe completar todos los campos para agregar"
+            }
+
+            if (products.some(product => product.code === code)) {
+                return "Ya existe un producto con este código"
+            }
+
+            let id = 1
+            if (products.length > 0) {
+                id = products[products.length - 1].id + 1
+            }
+
+            const newProduct = { id, title, description, price, thumbnail, stock, code };
+            products.push(newProduct);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5));
+            return "Producto agregado correctamente";
+        } catch (error) {
+            return error.message;
         }
-
-        if (products.some(product => product.code === code)) {
-            return "Ya existe un producto con este código"
-        }
-
-        let id = 1
-        if (products.length > 0) {
-            id = products[products.length - 1].id + 1
-        }
-
-        const newProduct = { id, title, description, price, thumbnail, stock, code };
-        products.push(newProduct);
-
-        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5));
-        return "Producto agregado correctamente";
     }
 
     async updateProduct(idProduct, datos = {}) {
-
+        try {
         let propiedadesValidasProduct = ["title", "description", "price", "thumbnail", "stock", "code"]
 
         let propiedadesQueQuieroModificar = Object.keys(datos)
@@ -58,34 +62,50 @@ class ProductManager {
 
         await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5));
         return "Producto modificado correctamente";
+
+        } catch (error) {
+            return error.message;
+        }
     }
 
     async deleteProduct(idProduct) {
-        let products = await this.getProducts();
-        let filteredProducts = products.filter(product => product.id !== idProduct);
-
-        if (filteredProducts.length === products.length) {
-            return "No se encontró ningún producto con ese ID";
+        try {
+            let products = await this.getProducts();
+            let filteredProducts = products.filter(product => product.id !== idProduct);
+    
+            if (filteredProducts.length === products.length) {
+                return "No se encontró ningún producto con ese ID";
+            }
+    
+            await fs.promises.writeFile(this.path, JSON.stringify(filteredProducts, null, 5));
+            return "Producto eliminado correctamente";
+        } catch (error) {
+            return error.message;
         }
-
-        await fs.promises.writeFile(this.path, JSON.stringify(filteredProducts, null, 5));
-        return "Producto eliminado correctamente";
     }
 
     async getProducts() {
-        if (fs.existsSync(this.path)) {
-            const data = await fs.promises.readFile(this.path, { encoding: "utf-8" });
-            return JSON.parse(data);
-        } else {
-            return [];
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, { encoding: "utf-8" });
+                return JSON.parse(data);
+            } else {
+                return [];
+            }
+        } catch (error) {
+            return error.message;
         }
     }
 
     async getProductById(id) {
-        const products = await this.getProducts();
-        const product = products.find(p => p.id == id);
+        try {
+            const products = await this.getProducts();
+            const product = products.find(p => p.id == id);
 
-        return product ? product : -1;
+            return product ? product : -1;
+        } catch (error) {
+            return error.message;
+        }
     }
 }
 
